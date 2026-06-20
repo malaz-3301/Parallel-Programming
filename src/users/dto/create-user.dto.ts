41cd08178c4862a000a2b972f981dee76e0ae088
Mohammad Malaz Ahmad <lozo4hk@gmail.com>
2026-06-20 07:46:39 +0300

    Validate mobile numbers using Syrian locale

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,24 @@
-import { IsMobilePhone, IsString, IsStrongPassword, MaxLength } from "class-validator"
+import {
+  IsMobilePhone,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @MaxLength(100)
-    @IsString()
-    name!: string
-    @IsStrongPassword()
-    @MaxLength(100)
-    password!: string
-    @IsMobilePhone()
-    @MaxLength(10)
-    phone!: string
-    @IsString()
-    @MaxLength(100)
-    photo!: string
+  @MaxLength(100)
+  @IsString()
+  name!: string;
+
+  @IsStrongPassword()
+  @MaxLength(100)
+  password!: string;
+
+  @IsMobilePhone('ar-SY')
+  @MaxLength(10)
+  phone!: string;
+
+  @IsString()
+  @MaxLength(100)
+  photo!: string;
 }
