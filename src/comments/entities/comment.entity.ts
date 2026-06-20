e857870c4e0eb4ff3e12401a9fa69df867c757b5
Mohammad Malaz Ahmad <lozo4hk@gmail.com>
2026-06-20 07:36:36 +0300

    Add safe cascade deletes for comments

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,18 +1,29 @@
-import { Product } from "src/products/entities/product.entity"
-import { User } from "src/users/entities/user.entity"
-import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, VersionColumn } from "typeorm"
+import { Product } from 'src/products/entities/product.entity';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
 @Entity()
 export class Comment {
-    @PrimaryGeneratedColumn()
-    id!: number
-    @Column()
-    description!: string
-    @Column()
-    rating!: number
-    @ManyToOne(()=> User)
-    @JoinColumn()
-    user! : User
-    @ManyToOne(()=> Product)
-    @JoinColumn()
-    product! : Product
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  description!: string;
+
+  @Column()
+  rating!: number;
+
+  @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+  @JoinColumn()
+  user!: User;
+
+  @ManyToOne(() => Product, { nullable: false, onDelete: 'CASCADE' })
+  @JoinColumn()
+  product!: Product;
 }
