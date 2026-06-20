87c5f1451ac9c67edcc98678ca81ca8c11419b03
Mohammad Malaz Ahmad <lozo4hk@gmail.com>
2026-06-20 08:28:44 +0300

    Expose system resources endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -19,4 +19,10 @@ export class AppController {
   getInstance() {
     return this.appService.getInstance();
   }
+
+  @Public()
+  @Get('resources')
+  getResources() {
+    return this.appService.getResources();
+  }
 }
